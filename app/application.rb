d34b1d8571df992acdabc9fd2c35af919e00b24d
dustinlikens <dustinlikens@icloud.com>
2017-02-08 00:47:09 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,14 +9,13 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split('/items/')[1]
-      item = @@items.detect{|i|i.name == item_name}
-        if item
-          resp.status = 200
-          resp.write item.price
-        else
-          resp.status = "400"
-          resp.write "Item not found"
-        end
+      if item = @@items.detect{|i|i.name == item_name}
+        resp.status = 200
+        resp.write item.price
+      else
+        resp.status = "400"
+        resp.write "Item not found"
+      end
     else
       resp.status = "404"
       resp.write "Route not found"
