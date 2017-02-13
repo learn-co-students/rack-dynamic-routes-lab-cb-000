71076ec42e62a06e549b22d6c1662096fcebfafe
Jesus Vega <chuyspc@gmail.com>
2017-02-13 09:43:25 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,20 @@
 class Application
-
-  @@items = []
+  @@items = [Item.new("Monkey Plush", 18.34) , Item.new("Apple", 3.42) , Item.new("Ipod" , 199.99)]
 
   def call(env)
+
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      item = req.path.split("/items/").last
-      resp.write handle_item(item)
-      if handle_item(item)=="Item not found"
-        resp.status = 400
+    if req.path == "/items/"
+      item_param = req.path.split('/items/').last
+
+      resp.write check_item(item_param)
+
+      if check_item(item_param) == "Item not found"
+        resp.status = 400 # Item not found
       else
-        resp.status = 200
+        resp.status = 200 # Item found
       end
     else
       resp.write "Route not found"
@@ -22,14 +24,14 @@ class Application
     resp.finish
   end
 
-  def handle_item(search_term)
-    price="Item not found"
+  def check_item(item)
+    price = "Item not found"
     @@items.each do |i|
-      if i.name == search_term
+      if i.name == item
         price = "#{i.price}"
       end
     end
-    return price
+    price
   end
 
 end
