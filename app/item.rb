b6bc7e94dcc6d31071339e42150fdc3df274a718
Juha  Bach <cdalsoniii@gmail.com>
2017-02-26 21:11:05 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,22 @@
 class Item
   attr_accessor :name, :price
 
+  # @@all = []
+
   def initialize(name,price)
     @name = name
     @price = price
+    # @@all << self
   end
+
+  # def self.all?
+  #   @@all
+  # end
+
+  # def self.get_price_by_name(name)
+  #   item = self.all.detect{|i|i.name == name}
+  #   item.price
+  # end
+
+
 end
