208991aa5e65122605ff90f87b3fa35b5a8aa43d
adam n <adam.nawroc@gmail.com>
2018-03-21 11:06:01 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,22 @@
 class Application
   def call(env)
-      resp = Rack::Response.new
-      # resp.write "Hello, World!"
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-      # https://github.com/bcuz/rack-get-params-lab-cb-000/blob/master/app/application.rb
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = @@items.find{|s| s.name == item_name}
+
+      if !item.nil?
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
       resp.finish
     end
 end
