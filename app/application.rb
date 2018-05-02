3a2ce5388fa4f499bd5ddb49fd9801ceba43d719
Liz Jenerson <liz.jenerson@gmail.com>
2018-05-02 09:26:27 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -11,12 +11,18 @@ class Application
 
       #possibly something to do w/ params[]
       #code below works if there is no if condition
-      item_name = req.path.split("/items/").last
 
-      if @@items.include?(item_name)
-        item = @@items.find{|i| i.name == item_name}
-        resp.write item.price
+
+      item_name = req.path.split("/items/").last
+      puts item_name
+
+      item = @@items.find{|i| i.name == item_name}
+
+      if item == nil
+        resp.write "Item not found"
+        resp.status = 400
       else
+        resp.write item.price
       end
 
     else
