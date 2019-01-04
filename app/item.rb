f611b357ccfbd43c3044aee787fdba515bc15319
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-04 20:22:06 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,25 @@
 class Item
   attr_accessor :name, :price
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
   end
+
+  def self.find_by_name(name)
+    @@all.find do |item|
+      item.name == name
+    end
+  end
+
 end
+
+#gun = Item.new('glock', '$5')
+#puts Item.find_by_name('cock') == nil
+#puts Item.all
